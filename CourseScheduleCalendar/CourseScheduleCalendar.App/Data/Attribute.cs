551d546a0496e9629238614237816c93e41998d9
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-04-13 15:29:28 -0400

    Add entity models

---
+++ CourseScheduleCalendar/CourseScheduleCalendar.App/Data/Attribute.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseScheduleCalendar.App.Data
+{
+    public class Attribute
+    {
+        public int Id { get; set; }
+
+        public int SectionId { get; set; }
+
+        public Section Section { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ CourseScheduleCalendar/CourseScheduleCalendar.App/Data/Instructor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CourseScheduleCalendar.App.Data
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ICollection<SectionInstructor> SectionInstructors { get; set; }
+
+        public Instructor()
+        {
+            SectionInstructors = new HashSet<SectionInstructor>();
+        }
+    }
+}
