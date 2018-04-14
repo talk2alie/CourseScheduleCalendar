72da4a3223af915baa920959ccb91a1e54b980cd
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-04-14 19:30:08 -0400

    Add test for adding a new semester

---
+++ CourseScheduleCalendar/CourseScheduleCalendar.App/Repositories/ICourseRepository.cs
@@ -0,0 +1,29 @@
+﻿using CourseScheduleCalendar.App.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseScheduleCalendar.App.Repositories
+{
+    public interface ICourseRepository
+    {
+        void AddCourse(Course course);
+
+        void UpdateCourse(Course course);
+
+        Task<Course> GetCourseAsync(int courseId);
+
+        Task<Course> GetCourseAsync(string courseName);
+
+        Task AddSectionForCourseAsync(int courseId, Section section);
+
+        Task AddSectionForCourseAsync(string courseName, Section section);
+
+        Task<IEnumerable<Section>> GetSectionsForCourseAsync(int courseId);
+
+        Task<IEnumerable<Section>> GetSectionsForCourseAsync(string courseName);
+
+        Task CommitChangesAsync();
+    }
+}
---
+++ CourseScheduleCalendar/CourseScheduleCalendar.Tests/Repositories/SemesterRepositoryTests.cs
@@ -0,0 +1,51 @@
+﻿using CourseScheduleCalendar.App.Data;
+using CourseScheduleCalendar.App.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CourseScheduleCalendar.Tests.Repositories
+{
+    public  class SemesterRepositoryTests
+    {
+        private readonly CourseScheduleDbContext _courseScheduleDbContext;
+        private readonly ISemesterRepository _semesterRepository;
+
+        public SemesterRepositoryTests()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            _courseScheduleDbContext = new CourseScheduleDbContext(optionsBuilder.Options);
+            _courseScheduleDbContext.Database.EnsureDeleted();
+
+            _semesterRepository = new SemesterRepository(_courseScheduleDbContext);
+        }
+
+        [Fact, Trait("Category", "SemesterRepository")]
+        public async Task AddSemester_Semester_IncreaseSemestersCountByOne()
+        {
+            // Arrange
+            var semester = new Semester
+            {
+                Description = "Programming for All",
+                Name = "CSC 1010",
+            };
+
+            const int addedSemestersCount = 1;
+            int expectedSemestersCount = await _courseScheduleDbContext.Semesters.CountAsync() + addedSemestersCount;
+
+            // Act
+            _semesterRepository.AddSemester(semester);
+            await _semesterRepository.CommitChangesAsync();
+
+            // Assert
+            int actualSemestersCount = await _courseScheduleDbContext.Semesters.CountAsync();
+
+            Assert.Equal(expectedSemestersCount, actualSemestersCount);
+        }
+    }
+}
